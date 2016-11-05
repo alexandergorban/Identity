f935aadeb205e775a5c9603090f5ff17678fd4af
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-11-05 05:34:50 +0200

    Identity and Security. [Authorize]

---
+++ Identity/Controllers/SecretController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace Identity.Controllers
+{
+    //[Authorize(Roles = "admin, sales")]
+    [Authorize(Users = "alex.gorban@outlook.com")]
+    public class SecretController : Controller
+    {
+        public ContentResult Secret()
+        {
+            return Content("This is a secret...");
+        }
+
+        [AllowAnonymous]
+        public ContentResult Over()
+        {
+            return Content("This is not a secret...");
+        }
+    }
+}
